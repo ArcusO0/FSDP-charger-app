22ba34af1cadd54df0a110d28cfdde3ef0af02a3
naqibam <naqibmrz@gmail.com>
2023-07-05 09:03:04 +0800

    add feedback

---
+++ client/src/pages/Status.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Typography, Grid, Card, CardContent, Input, IconButton, Button } from '@mui/material';
+import { AccessTime, Search, Clear, Edit } from '@mui/icons-material';
+import http from '../http';
+import dayjs from 'dayjs';
+import global from '../global';
+
+function Status() {
+    <Box>
+        <h3>Time Elapsed</h3>
+        <h1>00:00:00</h1>
+    </Box>    
+}
+
+export default Status;
