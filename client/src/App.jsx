a12baa56608c3459f7b68a88f26d20b16b38bc0f
ArcusO0 <marcusloh05@gmail.com>
2023-08-02 22:20:39 +0800

    add map to homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import AdminHome from './pages/AdminHome';
 import AdminCharger from './pages/Chargers';
 import AdminBookings from './pages/bookings';
-import AdminRequests from './pages/requests';
+import AdminRequests from './pages/Requests';
+import HomePage from './pages/home';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Container>
         <Routes>
           <Route path={"/"} element={<AdminHome />} />
+          <Route path={"/home"} element={<HomePage/>} />
           <Route path={"/AdminCharger"} element={<AdminCharger />} />
           <Route path={"/AdminBookings"} element={<AdminBookings />} />
           <Route path={"/Requests"} element={<AdminRequests />} />
